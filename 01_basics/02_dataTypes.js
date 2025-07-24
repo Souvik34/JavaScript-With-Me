814df46180011c3a6e904b8a292cc96686daa8c3
Souvik <souvik13109@gmail.com>
2025-07-24 21:37:03 +0530

    Data types in JavaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use-strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node.js, not browser
+
+
+
+console.log(3+3);
+
+let name = "Souvik";
+let age = 25
+let isEmployee = true;
+
+// ----------Data Types in JavaScript----------
+// number => 2^53 
+//bigint => 2^64
+//string=> ""
+//boolean => true/false
+//null => standalone value, no type
+//undefined => variable is declared but not assigned a value
+//symbol =>unique 
+
+
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof false);
+console.log(typeof "Sam");
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,3 @@
 console.log("Lets learn JavaScript!");
+
+
